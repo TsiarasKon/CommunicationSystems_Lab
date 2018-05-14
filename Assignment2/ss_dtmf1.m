df7b74c9734eea68dfd74d92d5a7ed633c1983a5
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2018-05-14 23:54:58 +0300

    As2: Ex1

---
+++ Assignment2/ss_dtmf1.m
@@ -0,0 +1,26 @@
+function [x] = ss_dtmf1(key, t, Ts)
+    f1_all = [697 770 852 941];
+    f2_all = [1209 1336 1477];
+    if key > 12
+        disp('Invalid key.');
+        return
+    elseif key == 12
+        f1 = 0;
+        f2 = 0;
+    elseif key == 11
+        f1 = f1_all(4);
+        f2 = f2_all(3);
+    elseif key == 10
+        f1 = f1_all(4);
+        f2 = f2_all(1);
+    elseif key == 0
+        f1 = f1_all(4);
+        f2 = f2_all(2);
+    else
+        f1 = f1_all(floor((key - 1) / 3) + 1);
+        f2 = f2_all(mod((key - 1), 3) + 1);
+    end
+    time = 0:Ts:t;
+    x = sin(2*pi*f1*time) + sin(2*pi*f2*time);
+end
+
